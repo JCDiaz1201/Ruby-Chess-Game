c6aad0630314fb22f44f7f75589af1d2e1744ec2
JDjedi <JDjedi1201@gmail.com>
2016-09-08 20:41:47 -0500

    Added some comments

--- chess.rb
+++ chess.rb
@@ -1,20 +1,23 @@
 class GameBoardSetup
-	def game_board
+	def game_board #define and set up the board
 		board_arr = [
 			["", "A", "B", "C", "D", "E", "F", "G", "H"],
-			[1, "A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1"],
-			[2, "A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2"],
-			[3, "A3", "B3", "C3", "D3", "E3", "F3", "G3", "H3"],
-			[4, "A4", "B4", "C4", "D4", "E4", "F4", "G4", "H4"],
-			[5, "A5", "B5", "C5", "D5", "E5", "F5", "G5", "H5"],
-			[6, "A6", "B6", "C6", "D6", "E6", "F6", "G6", "H6"],
-			[7, "A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7"],
-			[8, "A8", "B8", "C8", "D8", "E8", "F8", "G8", "H8"]
+			[1, "*", "*", "*", "*", "*", "*", "*", "*"],
+			[2, "*", "*", "*", "*", "*", "*", "*", "*"],
+			[3, "*", "*", "*", "*", "*", "*", "*", "*"],
+			[4, "*", "*", "*", "*", "*", "*", "*", "*"],
+			[5, "*", "*", "*", "*", "*", "*", "*", "*"],
+			[6, "*", "*", "*", "*", "*", "*", "*", "*"],
+			[7, "*", "*", "*", "*", "*", "*", "*", "*"],
+			[8, "*", "*", "*", "*", "*", "*", "*", "*"]
 		]
 		board_assignments(board_arr)
 	end
 
-	def board_assignments(arg)
+	def board_assignments(arg) #pass each playable sector of the board to itw own unique variable
+
+		puts arg 
+		
 		a1 = arg[1][1]
 		a2 = arg[2][1]
 		a3 = arg[3][1]
@@ -86,8 +89,6 @@ class GameBoardSetup
 		h6 = arg[6][8]
 		h7 = arg[7][8]
 		h8 = arg[8][8]
-
-		puts h7
 	end
 end
 
