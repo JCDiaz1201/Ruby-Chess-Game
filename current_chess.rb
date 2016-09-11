fd9d533d59dd87f5f76311035ab10aecce150c9a
JDjedi <JDjedi1201@gmail.com>
2016-09-10 19:59:26 -0500

    Amended current_chess.rb

--- current_chess.rb
+++ current_chess.rb
@@ -2,7 +2,7 @@ class GameBoardSetup
 	attr_accessor :board_array, :board_hash
 
 	def initialize
-			[" ", "A", "B", "C", "D", "E", "F", "G", "H"] #just here to help me visualize the game board
+#			[" ", "A", "B", "C", "D", "E", "F", "G", "H"] #just here to help me visualize the game board
 #			["1", "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"]
 #			["2", "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"]
 #			["3", "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"]
