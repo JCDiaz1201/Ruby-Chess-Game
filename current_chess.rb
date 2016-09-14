30a78be8095c959fbcd4f81e8541706515d0e8d9
JDjedi <JDjedi1201@gmail.com>
2016-09-14 18:54:38 -0500

    Amended chess.rb

--- current_chess.rb
+++ current_chess.rb
@@ -65,11 +65,19 @@ class Movement
 		if self.game_board.board_hash[piece_to_where.to_sym].nil?
 			puts "Error, please select a valid spot on the board"
 			end_spot(arg)
+		elsif self.game_board.board_hash[arg.to_sym] == @pawn; @rook; @knight; @bishop; @queen; @king
+			kill(arg, piece_to_where)
 		else
 			self.game_board.board_hash[piece_to_where.to_sym] = self.game_board.board_hash[arg.to_sym]
-			self.game_board.board_hash[arg.to_sym] = ""
+			self.game_board.board_hash[arg.to_sym] = "*"
 			self.game_board.board_hash.each {|v| puts "#{v}"}
 		end
+	end
+
+	def kill(arg, arg2)
+		self.game_board.board_hash[arg.to_sym] = "*"
+		self.game_board.board_hash[arg2.to_sym] == self.game_board.board_hash[arg.to_sym]
+		self.game_board.board_hash.each {|v| puts "#{v}"}
 	end
 end
 
