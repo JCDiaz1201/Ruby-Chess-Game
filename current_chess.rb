48b8176ea13b188bbe4259a6a372a7ead7b27e1f
JDjedi <JDjedi1201@gmail.com>
2016-09-13 20:51:46 -0500

    Amended chess.rb

--- current_chess.rb
+++ current_chess.rb
@@ -1,3 +1,15 @@
+# here for visualization purposes
+#
+# :a1 :b1 :c1 :d1 :e1 :f1 :g1 :h1
+# :a2 :b2 :c2 :d2 :e2 :f2 :g2 :h2
+# :a3 :b3 :c3 :d3 :e3 :f3 :g3 :h3
+# :a4 :b4 :c4 :d4 :e4 :f4 :g4 :h4
+# :a5 :b5 :c5 :d5 :e5 :f5 :g5 :h5
+# :a6 :b6 :c6 :d6 :e6 :f6 :g6 :h6
+# :a7 :b7 :c7 :d7 :e7 :f7 :g7 :h7
+# :a8 :b8 :c8 :d8 :e8 :f8 :g8 :h8
+#
+
 class GameBoardSetup
 	attr_accessor :board_hash, :pawn, :rook, :knight, :bishop, :queen, :king
 
@@ -23,25 +35,8 @@ class GameBoardSetup
 			:g8 => @knight, :h1 => "*", :h2 => "*", :h3 => "*", :h4 => "*", 
 			:h5 => "*", :h6 => "*", :h7 => @pawn, :h8 => @rook
 		}
-
-
-
 	end
 end
-
-# here for visualization purposes
-#
-# :a1 :b1 :c1 :d1 :e1 :f1 :g1 :h1
-# :a2 :b2 :c2 :d2 :e2 :f2 :g2 :h2
-# :a3 :b3 :c3 :d3 :e3 :f3 :g3 :h3
-# :a4 :b4 :c4 :d4 :e4 :f4 :g4 :h4
-# :a5 :b5 :c5 :d5 :e5 :f5 :g5 :h5
-# :a6 :b6 :c6 :d6 :e6 :f6 :g6 :h6
-# :a7 :b7 :c7 :d7 :e7 :f7 :g7 :h7
-# :a8 :b8 :c8 :d8 :e8 :f8 :g8 :h8
-#
-
-
 
 class Movement
 	attr_accessor :game_board
