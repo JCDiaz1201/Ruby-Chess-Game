c4b1ac52bdce52477670cbd4eaa3c5d8ecc61858
JDjedi <JDjedi1201@gmail.com>
2016-10-17 17:59:57 -0500

    Put correct pieces to spaces on game board

--- chess.rb
+++ chess.rb
@@ -24,12 +24,12 @@ class GameBoardSetup
 		@board_hash = {
 			:empty => "+", :a => "a", :b => "b", :c => "c", :d => "d", :e => "e", :f => "f", :g => "g", :h => "h",
 			:r1 => "1", :a1 => Rook.new("a1", "Black"), :b1 => Knight.new("b1", "Black"), :c1 => Bishop.new("c1", "Black"), :d1 => Queen.new("d1", "Black"), :e1 => King.new("e1", "Black"), :f1 => Bishop.new("f1", "Black"), :g1 => Knight.new("g1", "Black"), :h1 => Rook.new("h1", "Black"),
-			:r2 => "2", :a2 => "*", :b2 => "*", :c2 => "*", :d2 => Pawn.new("d2", "Black"), :e2 => "*", :f2 => "*", :g2 => "*", :h2 => "*",
+			:r2 => "2", :a2 => Pawn.new("a2", "Black"), :b2 => Pawn.new("b2", "Black"), :c2 => Pawn.new("c2", "Black"), :d2 => Pawn.new("d2", "Black"), :e2 => Pawn.new("e2", "Black"), :f2 => Pawn.new("f2", "Black"), :g2 => Pawn.new("g2", "Black"), :h2 => Pawn.new("h2", "Black"),
 			:r3 => "3", :a3 => "*", :b3 => "*", :c3 => "*", :d3 => "*", :e3 => "*", :f3 => "*", :g3 => "*", :h3 => "*",
 			:r4 => "4", :a4 => "*", :b4 => "*", :c4 => "*", :d4 => "*", :e4 => "*", :f4 => "*", :g4 => "*", :h4 => "*",
 			:r5 => "5", :a5 => "*", :b5 => "*", :c5 => "*", :d5 => "*", :e5 => "*", :f5 => "*", :g5 => "*", :h5 => "*",
 			:r6 => "6", :a6 => "*", :b6 => "*", :c6 => "*", :d6 => "*", :e6 => "*", :f6 => "*", :g6 => "*", :h6 => "*",
-			:r7 => "7", :a7 => "*", :b7 => Pawn.new("b7", "White"), :c7 => "*", :d7 => Pawn.new("d7", "Black"), :e7 => "*", :f7 => "*", :g7 => "*", :h7 => "*",
+			:r7 => "7", :a7 => Pawn.new("a7", "White"), :b7 => Pawn.new("b7", "White"), :c7 => Pawn.new("c7", "White"), :d7 => Pawn.new("d7", "White"), :e7 => Pawn.new("e7", "White"), :f7 => Pawn.new("f7", "White"), :g7 => Pawn.new("g7", "White"), :h7 => Pawn.new("h7", "White"),
 			:r8 => "8", :a8 => Rook.new("a8", "White"), :b8 => Knight.new("b8", "White"), :c8 => Bishop.new("c8", "White"), :d8 => King.new("d8", "White"), :e8 => Queen.new("e8", "White"), :f8 => Bishop.new("f8", "White"), :g8 => Knight.new("g8", "White"), :h8 => Rook.new("h8", "White")
 		}
 	end
